7cba6db92f2eff7e720bdffcf5c5684ccca9cd6d
Anvay Srivastava <anvaysrivastava@gmail.com>
2019-05-30 19:58:52 +0530

    Revert "read json from disk"

--- prashnavali/ViewController.swift
+++ prashnavali/ViewController.swift
@@ -8,30 +8,13 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
 
-
-    @IBOutlet weak var textLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
-                if let jsonResult = jsonResult as? Array<AnyObject> {
-                    // do stuff
-                    let sholkas = jsonResult[0]["shloka"] as! Array<String>
-                    textLabel.text = sholkas[0]
-                    
-                }
-            } catch {
-                // handle error
-            }
-        }
     }
+
+
 }
 
